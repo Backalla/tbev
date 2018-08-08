35e61ea0583f7343119bce941e3cbad0021d7f83
Tushar Pawar <tusharvijaypawar@gmail.com>
2018-08-08 15:15:33 +0530

    Accept embeddings and logdir as arguments

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tbev",
-    version="0.0.7",
+    version="0.0.8",
     author="Tushar Pawar",
     author_email="gmail@tusharpawar.com",
     description="Embedding Visualizer Using Tensorboard",
