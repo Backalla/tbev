ec7da49192be9a94abcef7dc4f7b75c3b02d8182
Tushar Pawar <tusharvijaypawar@gmail.com>
2018-08-08 15:20:00 +0530

    v0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tbev",
-    version="0.0.8",
+    version="0.0.9",
     author="Tushar Pawar",
     author_email="gmail@tusharpawar.com",
     description="Embedding Visualizer Using Tensorboard",
